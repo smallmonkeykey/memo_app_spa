4aa8bb6631791650eac51c5ab749f84e44d97265
smallmonkeykey <sarinitakuoo@gmail.com>
2024-07-03 09:58:40 +0900

    タイトルを表示

--- src/MemoRow.js
+++ src/MemoRow.js
@@ -1,12 +1,23 @@
-const Memos = [
-  { id: 1, title: "メモ1", content: "メモ1内容" },
-  { id: 2, title: "メモ2", content: "メモ2内容" },
-  { id: 3, title: "メモ3", content: "メモ3内容" },
-  { id: 4, title: "メモ4", content: "メモ4内容" },
-];
+import { useState } from "react";
 
 function MemoRow() {
-  const memoTitle = Memos.map((memo) => <li key={memo.id}>{memo.title}</li>);
+  const allMemos = function () {
+    const allMemos = [];
+
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const value = localStorage.getItem(key);
+      allMemos.push({ title: key, content: value });
+    }
+
+    return allMemos;
+  };
+
+  const [memoList] = useState(allMemos);
+
+  const memoTitle = memoList.map((memo) => (
+    <li key={memo.title}>{memo.title}</li>
+  ));
   return <ul>{memoTitle}</ul>;
 }
 
