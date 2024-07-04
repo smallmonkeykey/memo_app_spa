30b78af56c279dd0f16f46d719953f7c1b9b1ee5
smallmonkeykey <sarinitakuoo@gmail.com>
2024-07-04 10:01:03 +0900

    新しいメモを追加する

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import MemoTable from "./MemoRow.js";
-import InputMemo from "./InputMemo.js";
 
 function App() {
   return (
     <div>
       <MemoTable />
-      <InputMemo />
     </div>
   );
 }
--- src/MemoRow.js
+++ src/MemoRow.js
@@ -1,23 +1,9 @@
 import { useState } from "react";
+import { useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function MemoRow() {
-  const allMemos = function () {
-    const allMemos = [];
-
-    for (let i = 0; i < localStorage.length; i++) {
-      const key = localStorage.key(i);
-      const value = localStorage.getItem(key);
-      allMemos.push({ title: key, content: value });
-    }
-
-    return allMemos;
-  };
-
-  const [memoList] = useState(allMemos);
-
-  const memoTitle = memoList.map((memo) => (
-    <li key={memo.title}>{memo.title}</li>
-  ));
+function MemoRow({ memoList }) {
+  const memoTitle = memoList.map((memo) => <li key={memo.id}>{memo.title}</li>);
   return <ul>{memoTitle}</ul>;
 }
 
@@ -32,11 +18,53 @@ function AddMemo() {
   );
 }
 
+function InputMemo({ memoList, setMemoList }) {
+  const inputTextRef = useRef();
+
+  const handleAddMemo = (e) => {
+    e.preventDefault();
+    const inputMemo = inputTextRef.current.value;
+    const newMemo = {
+      id: uuidv4(),
+      title: inputMemo.split("\n")[0],
+      content: inputMemo,
+    };
+    localStorage.setItem(newMemo.id, newMemo.content);
+    inputTextRef.current.value = "";
+
+    setMemoList([...memoList, newMemo]);
+  };
+
+  return (
+    <form>
+      <textarea rows="20" cols="50" ref={inputTextRef}></textarea>
+      <button type="submit" onClick={handleAddMemo}>
+        追加
+      </button>
+    </form>
+  );
+}
+
+
 export default function MemoTable() {
+  const getAllMemos = () => {
+    const allMemos = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const value = localStorage.getItem(key);
+      const title = value.split("\n")[0];
+      allMemos.push({ id: key, title: title, content: value });
+    }
+    return allMemos;
+  };
+
+  const [memoList, setMemoList] = useState(getAllMemos());
+
   return (
     <>
-      <MemoRow />
+      <MemoRow memoList={memoList} />
       <AddMemo />
+      <InputMemo memoList={memoList} setMemoList={setMemoList} />
     </>
   );
 }
